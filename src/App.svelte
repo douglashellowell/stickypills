1726946fc3779876027d778c79caa9765e17bd0b
Douglas Hellowell <doog99@gmail.com>
2022-06-02 17:42:45 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Sticky Pills</h1>
+
+  <section>
+    <h2>menu</h2>
+    <ul>
+      <li>play</li>
+      <li>rules</li>
+    </ul>
+  </section>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/lib/Card/Card.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div>
+  <p>drug name fragment</p>
+  <p>Main-Effect</p>
+  <p>Side-Effect</p>
+</div>
